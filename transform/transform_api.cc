02727096b1a9ba00facd9a1f775353e04427ad65
sunlh <200359659@qq.com>
2019-05-06 12:27:19 +0000

    add extension event(50%)

--- transform/transform_api.cc
+++ transform/transform_api.cc
@@ -1,7 +1,15 @@
 #include "chrome/browser/extensions/api/transform/transform_api.h"
 
+#include <string>
+
+#include "chrome/browser/extensions/extension_tab_util.h"
 #include "extensions/browser/extension_function_registry.h"
+#include "extensions/browser/extension_function.h"
 #include "extensions/common/extension.h"
+
+#include "chrome/browser/ui/browser_commands.h"
+#include "chrome/browser/ui/browser_finder.h"
+#include "chrome/app/chrome_command_ids.h"
 
 namespace extensions {
 namespace api {
@@ -10,8 +18,37 @@ TransformOpenDialogFunction::TransformOpenDialogFunction() {}
 TransformOpenDialogFunction::~TransformOpenDialogFunction() {}
 
 bool TransformOpenDialogFunction::RunAsync() {
-  std::unique_ptr<transform::OpenDialog::Params> params(transform::OpenDialog::Params::Create(*args_));
+  std::unique_ptr<transform::OpenDialog::Params> params(
+      transform::OpenDialog::Params::Create(*args_));
+  EXTENSION_FUNCTION_VALIDATE(params.get());
+
+  Browser* browser = chrome::FindLastActive();
+
+  if (browser) {
+    chrome::ExecuteCommand(browser, IDC_SHOW_DOWNLOADS);
+  }
+
   return true;
+}
+
+ExtensionTransformEventRouter::ExtensionTransformEventRouter(
+    Profile* profile,
+    scoped_refptr<const Extension> extension)
+    : profile_(profile),
+      extension_(extension) {
+}
+
+void ExtensionTransformEventRouter::SendStartTransform() {
+  if (!EventRouter::Get((content::BrowserContext*)profile_))
+    return;
+
+  auto event = std::make_unique<extensions::Event>(
+      events::START_TRANSFORM,
+      OnStartTransform::kEventName,
+      std::unique_ptr<base::ListValue>(),
+      profile_);
+
+  EventRouter::Get((content::BrowserContext*)profile_)->DispatchEventToExtension(extension_->id(), std::move(event));
 }
 
 } // namespace api
--- transform/transform_api.h
+++ transform/transform_api.h
@@ -3,6 +3,11 @@
 
 #include "chrome/common/extensions/api/transform.h"
 #include "chrome/browser/extensions/chrome_extension_function.h"
+#include "extensions/common/extension.h"
+#include "extensions/browser/event_router.h"
+// #include "extensions/browser/extension_host.h"
+#include "extensions/browser/extension_registry.h"
+#include "extensions/browser/extension_registry_observer.h"
 
 namespace extensions {
 namespace api {
@@ -13,10 +18,28 @@ class TransformOpenDialogFunction : public ChromeAsyncExtensionFunction {
 
   protected:
     ~TransformOpenDialogFunction() override;
-    virtual bool RunAsync() override;
+    bool RunAsync() override;
 
   private:
     DECLARE_EXTENSION_FUNCTION("transform.openDialog", TRANSFORM_OPENDIALOG)
+};
+
+class ExtensionTransformEventRouter
+  : public extensions::EventRouter::Observer,
+    public extensions::ExtensionRegistryObserver {
+  public:
+    ExtensionTransformEventRouter(Profile* profile,
+                                  scoped_refptr<const Extension> extension);
+
+    ~ExtensionTransformEventRouter() override;
+
+    void SendStartTransform();
+
+  private:
+    Profile* profile_;
+    scoped_refptr<const Extension> extension_;
+
+    DISALLOW_COPY_AND_ASSIGN(ExtensionTransformEventRouter);
 };
 
 }  // namespace api
